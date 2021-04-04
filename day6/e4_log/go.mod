8433a45d2b095439c7901bd31720cb4e50e928f5
imw7 <hugew7@gmail.com>
2021-04-05 06:49:35 +0800

    log demo

---
+++ day6/e4_log/go.mod
@@ -0,0 +1,3 @@
+module log_demo
+
+go 1.16
---
+++ day6/e4_log/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+// log demo
+
+func main() {
+	fileObj, err := os.OpenFile("./xx.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("open file failed, err: ", err)
+		return
+	}
+	log.SetOutput(fileObj)
+	for {
+		log.Println("这是一条测试的日志")
+		time.Sleep(time.Second * 3)
+	}
+}
