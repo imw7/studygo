293d5b3df28f616188bcb552a2a4bbf7d39687f8
imw7 <hugew7@gmail.com>
2021-04-05 05:02:56 +0800

    closure

---
+++ day3/e9_closure/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 闭包
+
+func f1(f func()) {
+	fmt.Println("this is f1")
+	f()
+}
+
+func f2(x, y int) {
+	fmt.Println("this is f2")
+	fmt.Println(x + y)
+}
+
+// 要求：f1(f2)
+
+func f3(f func(int, int), x, y int) func() {
+	return func() {
+		f(x, y)
+	}
+}
+
+func main() {
+	ret := f3(f2, 100, 200) // 把原来需要传递两个int类型的参数包装成一个不需要传参的函数
+	f1(ret)
+}
