11a2dfbc34b0854a2179e992c1c6b4fffd67c029
imw7 <hugew7@gmail.com>
2021-04-05 09:21:07 +0800

    context.WithCancel

--- day11/e4_withcancel/main.go
+++ day11/e4_withcancel/main.go
@@ -26,7 +26,6 @@ func gen(ctx context.Context) <-chan int {
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel() // 当我们取完需要的整数后调用cancel
-
 	for n := range gen(ctx) {
 		fmt.Println(n)
 		if n == 5 {
