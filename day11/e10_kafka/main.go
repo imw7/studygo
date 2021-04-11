84000bc711a7a6386b697872935002f76819cac8
imw7 <hugew7@gmail.com>
2021-04-11 17:46:18 +0800

    delete annotation

--- day11/e10_kafka/main.go
+++ day11/e10_kafka/main.go
@@ -9,7 +9,6 @@ import (
 
 func main() {
 	config := sarama.NewConfig()
-	// tailf包使用
 	config.Producer.RequiredAcks = sarama.WaitForAll          // 发送完数据需要leader和follow都确认
 	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
 	config.Producer.Return.Successes = true                   // 成功交付的消息将在success channel返回
