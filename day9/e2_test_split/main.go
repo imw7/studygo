6e6105210d5c130e90aeceafb4d927982a6328d7
imw7 <hugew7@gmail.com>
2021-04-25 21:09:22 +0800

    change test case

--- day9/e2_test_split/main.go
+++ day9/e2_test_split/main.go
@@ -9,12 +9,12 @@ func main() {
 	ret1 := split.Split("a:b:c", ":")
 	fmt.Printf("%#v\n", ret1)
 
-	ret2 := split.Split("abcbebdbf", "b")
+	ret2 := split.Split("a:b:c", ",")
 	fmt.Printf("%#v\n", ret2)
 
 	ret3 := split.Split("about", "bo")
 	fmt.Printf("%#v\n", ret3)
 
-	ret4 := split.Split("上海自来水来自海上", "自")
+	ret4 := split.Split("上海水海上来", "上")
 	fmt.Printf("%#v\n", ret4)
 }
