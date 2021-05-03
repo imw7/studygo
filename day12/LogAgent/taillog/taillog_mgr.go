a4da90af1ec55ecefd090349f24fd901584abd76
imw7 <hugew7@gmail.com>
2021-05-03 18:45:51 +0800

    实现新增收集任务

--- day12/LogAgent/taillog/taillog_mgr.go
+++ day12/LogAgent/taillog/taillog_mgr.go
@@ -24,7 +24,10 @@ func Init(logEntries []*etcd.LogEntry) {
 	for _, logEntry := range logEntries {
 		// conf: *etcd.LogEntry
 		// logEntry.Path: 要收集的日志文件的路径
-		NewTailTask(logEntry.Path, logEntry.Topic)
+		// 初始化的时候起了多少个tailTask都要记下来，为了后续判断方便
+		tailTask := NewTailTask(logEntry.Path, logEntry.Topic)
+		pt := fmt.Sprintf("%s_%s", logEntry.Path, logEntry.Topic)
+		tskMgr.tskMap[pt] = tailTask
 	}
 	go tskMgr.run()
 }
@@ -34,9 +37,21 @@ func (t *tailLogMgr) run() {
 	for {
 		select {
 		case newConf := <-t.newConfChan:
-			// 1.配置新增
-			// 2.配置删除
-			// 3.配置变更
+			for _, conf := range newConf {
+				pt := fmt.Sprintf("%s_%s", conf.Path, conf.Topic)
+				_, ok := t.tskMap[pt]
+				if ok {
+					// 如果存在，不需要操作
+					continue
+				} else {
+					// 如果不存在的，当做新增的
+					tails := NewTailTask(conf.Path, conf.Topic)
+					t.tskMap[pt] = tails
+				}
+			}
+			// TODO 找出原来t.logEntry有，但是newConf中没有的，要删掉
+
+			// 配置删除
 			fmt.Println("new config is here~", newConf)
 		default:
 			time.Sleep(time.Second)
