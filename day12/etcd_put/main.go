9396c00fc0304953c170dc1c9d8a4d149bc3482f
imw7 <hugew7@gmail.com>
2021-05-03 14:50:03 +0800

    etcd put json value

---
+++ day12/etcd_put/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		// handle error!
+		fmt.Println("connect to etcd failed, err:", err)
+		return
+	}
+	fmt.Println("connect to etcd succeed.")
+	defer func() {
+		err := cli.Close()
+		if err != nil {
+			return
+		}
+	}()
+	// put
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	value := `[{"path":"/home/tmp/nginx.log","topic":"web_log"},{"path":"/usr/xxx/nginx.log","topic":"redis_log"}]`
+	_, err = cli.Put(ctx, "/xxx", value)
+	cancel()
+	if err != nil {
+		fmt.Println("put to etcd failed, err:", err)
+		return
+	}
+}
