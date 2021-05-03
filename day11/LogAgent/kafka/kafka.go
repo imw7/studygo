072ded137b76ce52401841a5e741155096613b78
imw7 <hugew7@gmail.com>
2021-05-03 13:07:34 +0800

    配置文件版LogAgent

---
+++ day11/LogAgent/kafka/kafka.go
@@ -0,0 +1,41 @@
+package kafka
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+// 专门往kafka写日志的模块
+
+var producer sarama.SyncProducer // 声明一个全局的连接kafka的生产者producer
+
+// Init 初始化client
+func Init(address []string) (err error) {
+	config := sarama.NewConfig()
+	// tailf包使用
+	config.Producer.RequiredAcks = sarama.WaitForAll          // 发送完数据需要leader和follow都确认
+	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
+	config.Producer.Return.Successes = true                   // 成功交付的消息将在success channel返回
+
+	// 连接Kafka
+	producer, err = sarama.NewSyncProducer(address, config)
+	if err != nil {
+		fmt.Println("producer closed, err:", err)
+		return
+	}
+	return
+}
+
+func SendToKafka(topic, data string) {
+	// 构造一个消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = topic
+	msg.Value = sarama.StringEncoder(data)
+	// 发送到Kafka
+	pid, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send message failed, err:", err)
+		return
+	}
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
