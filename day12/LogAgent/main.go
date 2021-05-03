622c1bfd49b66d0f362841b34ecdb46db63c7bb5
imw7 <hugew7@gmail.com>
2021-05-03 14:05:19 +0800

    etcd搭建

--- day12/LogAgent/main.go
+++ day12/LogAgent/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"LogAgent/conf"
+	"LogAgent/etcd"
 	"LogAgent/kafka"
-	"LogAgent/taillog"
 	"fmt"
 	"gopkg.in/ini.v1"
 	"time"
@@ -13,18 +13,18 @@ import (
 
 var cfg = new(conf.AppConf)
 
-func run() {
-	// 1.读取日志
-	for {
-		select {
-		case line := <-taillog.ReadChan():
-			// 2.发送到Kafka
-			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
-		default:
-			time.Sleep(time.Second)
-		}
-	}
-}
+// func run() {
+// 	// 1.读取日志
+// 	for {
+// 		select {
+// 		case line := <-taillog.ReadChan():
+// 			// 2.发送到Kafka
+// 			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
+// 		default:
+// 			time.Sleep(time.Second)
+// 		}
+// 	}
+// }
 
 func main() {
 	// 0.加载配置文件
@@ -42,14 +42,22 @@ func main() {
 	}
 	fmt.Println("init kafka succeed.")
 
-	// 2.打开日志文件准备收集日志
-	err = taillog.Init(cfg.TaillogConf.Filename)
+	// 2.初始化ETCD
+	err = etcd.Init(cfg.EtcdConf.Address, time.Duration(cfg.EtcdConf.Timeout)*time.Second)
 	if err != nil {
-		fmt.Println("init taillog failed, err:", err)
+		fmt.Println("init etcd failed, err:", err)
 		return
 	}
-	fmt.Println("init taillog succeed.")
+	fmt.Println("init etcd succeed.")
+
+	// 2.打开日志文件准备收集日志
+	// err = taillog.Init(cfg.TaillogConf.Filename)
+	// if err != nil {
+	// 	fmt.Println("init taillog failed, err:", err)
+	// 	return
+	// }
+	// fmt.Println("init taillog succeed.")
 
 	// 3.具体业务操作
-	run()
+	// run()
 }
