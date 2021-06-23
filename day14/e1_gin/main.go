606223abfdc27ad83b6c0272fae1178fec3219ab
imw7 <hugew7@gmail.com>
2021-06-23 21:42:43 +0800

    first gin

--- day14/e1_gin/main.go
+++ day14/e1_gin/main.go
@@ -9,11 +9,16 @@ import (
 
 func main() {
 	// 1.创建路由
+	// 默认使用了2个中间件Logger()，Recover()
 	r := gin.Default()
+	// 也可以创建不带中间件的路由
+	// r := gin.New()
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, world!")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, world!",
+		})
 	})
 	// 3.监听端口，默认在8080
 	_ = r.Run(":8000")
