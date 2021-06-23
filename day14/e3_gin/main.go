68962c7de4a513eeb607f19ae207ade4aa25ba5e
imw7 <hugew7@gmail.com>
2021-06-23 22:32:12 +0800

    gin get api param

--- day14/e3_gin/main.go
+++ day14/e3_gin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,6 +12,17 @@ func main() {
 	r.GET("/usr/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, name) // 浏览器输入localhost:8000/usr/eric 输出eric
+	})
+	r.GET("/usr/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		c.String(http.StatusOK, name+" is "+action)
+	})
+	r.GET("/welcome", func(c *gin.Context) {
+		// DefaultQuery第二个参数是默认值
+		// 想要取得不同的值需要在浏览器中输入localhost:8000/welcome?name=Sarah
+		name := c.DefaultQuery("name", "Eric")
+		c.String(http.StatusOK, fmt.Sprintf("Hello, %s.", name))
 	})
 	_ = r.Run(":8000")
 }
