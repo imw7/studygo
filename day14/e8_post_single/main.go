8cc1353b039a74b0113fb4d70905844afe075bc9
imw7 <hugew7@gmail.com>
2021-06-24 14:58:44 +0800

    gin upload single file

--- day14/e8_post_single/main.go
+++ day14/e8_post_single/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
 func main() {
-	$END$
+	r := gin.Default()
+	// 处理multipart forms提交文件时默认的内存限制是32 MiB
+	// 可以通过下面的方式修改
+	// r.MaxMultipartMemory = 8 << 20  // 8 MiB
+	r.POST("/upload", func(c *gin.Context) {
+		// 单个文件
+		file, err := c.FormFile("f1")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		log.Println(file.Filename)
+		dst := fmt.Sprintf("/tmp/%s", file.Filename)
+		// 上传文件到指定的目录
+		_ = c.SaveUploadedFile(file, dst)
+		// 打印信息
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("'%s' uploaded!", file.Filename),
+		})
+	})
+	_ = r.Run(":8000")
 }
