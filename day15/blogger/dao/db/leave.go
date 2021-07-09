e0d893661305f2a605663bf7b9e5b75fc45c6cbd
imw7 <hugew7@gmail.com>
2021-07-09 17:06:39 +0800

    留言的查询语句修复

--- day15/blogger/dao/db/leave.go
+++ day15/blogger/dao/db/leave.go
@@ -7,7 +7,7 @@ import (
 
 // InsertLeave 插入留言
 func InsertLeave(leave *model.Leave) (err error) {
-	sqlStr := "insert into leave(username, email, content) values (?, ?, ?)"
+	sqlStr := "insert into `leave`(username, email, content) values (?, ?, ?)"
 	_, err = DB.Exec(sqlStr, leave.Username, leave.Email, leave.Content)
 	if err != nil {
 		fmt.Printf("exec sql:%s failed, err:%v\n", sqlStr, err)
@@ -18,7 +18,7 @@ func InsertLeave(leave *model.Leave) (err error) {
 
 // GetLeaveList 获取留言
 func GetLeaveList() (leaveList []*model.Leave, err error) {
-	sqlStr := "select id, username, email, content, create_time from leave order by id desc"
+	sqlStr := "select id, username, email, content, create_time from `leave` order by id desc"
 	err = DB.Select(&leaveList, sqlStr)
 	if err != nil {
 		fmt.Printf("exec sql:%s failed, err:%v\n", sqlStr, err)
