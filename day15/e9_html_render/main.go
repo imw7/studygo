41f1b48832bfe8d25d4ed6663e5cd19af9ea01ae
imw7 <hugew7@gmail.com>
2021-07-10 19:42:38 +0800

    加载静态文件

--- day15/e9_html_render/main.go
+++ day15/e9_html_render/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
+
+	// 静态文件：html页面上用到的样式文件 .css js文件 图片
+
+	// 加载静态文件
+	r.Static("/xxx", "./statics")
+
+	// gin框架中给模板添加自定义函数
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(str string) template.HTML {
+			return template.HTML(str)
+		},
+	})
 	r.LoadHTMLGlob("templates/**/*") // 模板解析
-	// r.LoadHTMLFiles("templates/posts/index.tmpl", "templates/users/index.tmpl")
+	// r.LoadHTMLFiles("templates/posts/index.tmpl", "templates/users/index.tmpl", "templates/link.tmpl")
 	r.GET("/posts/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{ // 模板渲染
 			"title": "posts/index",
@@ -18,6 +31,12 @@ func main() {
 	r.GET("users/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
 			"title": "users/index",
+		})
+	})
+
+	r.GET("links/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "links/index.tmpl", gin.H{
+			"link": "<a href='https://www.baidu.com'>百度一下，你就知道</a>",
 		})
 	})
 
