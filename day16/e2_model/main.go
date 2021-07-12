9454a416d398e226845ae51d5792dc6a83556382
imw7 <hugew7@gmail.com>
2021-07-12 16:53:10 +0800

    定义模型

--- day16/e2_model/main.go
+++ day16/e2_model/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	_ "gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 	"time"
 )
 
@@ -23,34 +25,33 @@ type User struct {
 
 // Animal 使用`AnimalID`作为主键
 type Animal struct {
-	AnimalID int64  `gorm:"primary_key" gorm:"column:beast_id"`
+	AnimalID int64  `gorm:"primaryKey" gorm:"column:beast_id"`
 	Name     string `gorm:"column:name_of_the_beast"`
 	Age      int64  `gorm:"column:age_of_the_beast"` // 使用tag指定列名
 }
 
-// TableName 将 Animal 的表名设置为 `pets`
+// TableName 将 Animal 的表名设置为 `beasts`
 // 唯一指定表名
 func (Animal) TableName() string {
-	return "pets"
+	return "beasts"
 }
 
 func main() {
-	// 更改默认表名称规则
-	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-		return "prefix_" + defaultTableName
-	}
-
 	// 连接MySQL数据库
-	db, err := gorm.Open("mysql", "root:password@tcp(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := "root:password@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "prefix_", // 更改命名规则
+			SingularTable: true,      // 禁用复数
+		},
+	})
 	if err != nil {
 		panic(err)
 	}
-	defer func() { _ = db.Close() }()
-	db.SingularTable(true) // 禁用复数
 
-	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Animal{})
+	_ = db.AutoMigrate(&User{})
+	_ = db.AutoMigrate(&Animal{})
 
 	// 使用User结构体创建名为`deleted_users`的表
-	// db.Table("deleted_users").CreateTable(&User{})
+	_ = db.Table("deleted_users").AutoMigrate(&User{})
 }
