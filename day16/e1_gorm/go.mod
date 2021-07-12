cbc72c3815290fdba434d30152d1072eb330f454
imw7 <hugew7@gmail.com>
2021-07-12 16:23:42 +0800

    GORM快速入门

--- day16/e1_gorm/go.mod
+++ day16/e1_gorm/go.mod
@@ -2,4 +2,7 @@ module gorm_demo
 
 go 1.16
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.11
+)
--- day16/e1_gorm/main.go
+++ day16/e1_gorm/main.go
@@ -2,43 +2,54 @@ package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	_ "gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // GORM
 
-// UserInfo --> 数据表
-type UserInfo struct {
-	ID     uint
-	Name   string
-	Gender string
-	Hobby  string
+// Product --> 数据表
+type Product struct {
+	Code  string
+	Price uint
 }
 
 func main() {
 	// 连接MySQL数据库
-	db, err := gorm.Open("mysql", "root:password@tcp(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := "root:password@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		panic("failed to connect database")
 	}
-	defer func() { _ = db.Close() }()
 
 	// 创建表 自动迁移（把结构体和数据表进行对应）
-	db.AutoMigrate(&UserInfo{})
+	err = db.AutoMigrate(&Product{})
+	if err != nil {
+		panic("failed to auto migrate")
+	}
 
 	// 创建数据行
-	// u1 := UserInfo{ID: 1, Name: "梅西", Gender: "男", Hobby: "足球"}
-	// db.Create(&u1)
+	p := Product{Code: "C91", Price: 300}
+	db.Create(&p)
 
 	// 查询
-	var u UserInfo
-	db.First(&u) // 查询表中第一条数据保存到u中
-	fmt.Printf("u:%#v\n", u)
+	var product Product
+	db.First(&product, 1) // 根据整形主键查找
+	fmt.Printf("product:%v\n", product)
+	db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+	fmt.Printf("product:%v\n", product)
 
-	// 更新
-	db.Model(&u).Update("hobby", "烤肉")
+	// 更新 - 将 product 的 price 更新为 200
+	db.Model(&product).Update("Price", 200)
+	fmt.Printf("product:%v\n", product)
+	// 更新多个字段
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+	fmt.Printf("product:%v\n", product)
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	fmt.Printf("product:%v\n", product)
 
-	// 删除
-	db.Delete(&u)
+	// 删除 - 删除 product
+	db.Delete(&product, 1)
+	fmt.Printf("product:%v\n", product)
 }
