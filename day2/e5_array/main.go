3edb5c5748bb9bd54f211663d4499904c8209321
imw7 <hugew7@gmail.com>
2021-07-13 14:02:37 +0800

    数组补充

--- day2/e5_array/main.go
+++ day2/e5_array/main.go
@@ -64,4 +64,39 @@ func main() {
 	b2 := b1              // [1 2 3] Ctrl+C Ctrl+V
 	b2[0] = 100           // b2:[100 2 3]
 	fmt.Println(b1, b2)   // b1:? => [1 2 3]
+
+	// 数组指针
+	var a = [...]int{1, 2, 3} // a 是一个数组
+	var b = &a                // b 是指向数组的指针
+	fmt.Println(a[0], a[1])   // 打印数组的前2个元素
+	fmt.Println(b[0], b[1])   // 通过数组指针访问数组元素的方式和数组类似
+	fmt.Printf("b:%T\n", b)   // 打印类型
+	fmt.Printf("b:%#v\n", b)  // 打印详细信息
+
+	for i, v := range b { // 通过数组指针迭代数组的元素
+		fmt.Println(i, v)
+	}
+	// for range 忽略迭代时的下标
+	var times [5][0]int // 内存大小为0
+	for range times {
+		fmt.Println("hello")
+	}
+
+	// 长度为0的数组在内存中并不占用空间
+	// 我们并不关心管道中传输数据的真实类型
+	// 其中管道接收和发送操作只是用于消息的同步
+	// 用空数组来作为管道类型可以减少管道元素赋值时的开销
+	c1 := make(chan [0]int)
+	go func() {
+		fmt.Println("c1")
+		c1 <- [0]int{}
+	}()
+	<-c1
+	// 用无类型的匿名结构体代替长度为0的数组
+	c2 := make(chan struct{})
+	go func() {
+		fmt.Println("c2")
+		c2 <- struct{}{} // struct{}部分是类型，{}表示对应的结构体值
+	}()
+	<-c2
 }
