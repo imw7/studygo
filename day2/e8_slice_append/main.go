ec4e592e44210bc0bf8ea0adba4283fd8a7c7e3d
imw7 <hugew7@gmail.com>
2021-07-13 16:52:20 +0800

    在切片开头添加元素

--- day2/e8_slice_append/main.go
+++ day2/e8_slice_append/main.go
@@ -13,6 +13,7 @@ func main() {
 	// 调用append函数必须用原来的切片变量接收返回值
 	// append追加元素，原来的底层数组放不下的时候，Go底层就会把底层数组换一个
 	// 必须使用变量接收append的返回值
+	fmt.Printf("s1=%v len(s1)=%d cap(s1)=%d\n", s1, len(s1), cap(s1))
 	s1 = append(s1, "成都")
 	fmt.Printf("s1=%v len(s1)=%d cap(s1)=%d\n", s1, len(s1), cap(s1))
 	s1 = append(s1, "杭州", "武汉")
@@ -20,4 +21,13 @@ func main() {
 	ss := []string{"西安", "苏州", "厦门"}
 	s1 = append(s1, ss...) // ...表示拆开
 	fmt.Printf("s1=%v len(s1)=%d cap(s1)=%d\n", s1, len(s1), cap(s1))
+
+	// 在开头一般都会导致内存的重新分配，而且会导致已有的元素全部复制1次。
+	// 因此，从切片的开头添加元素的性能一般要比从尾部追加元素的性能差很多。
+	a := []int{1, 2, 3}
+	fmt.Printf("a=%v len(a)=%d cap(a)=%d\n", a, len(a), cap(a))
+	a = append([]int{0}, a...) // 在开头添加1个元素
+	fmt.Printf("a=%v len(a)=%d cap(a)=%d\n", a, len(a), cap(a))
+	a = append([]int{-3, -2, -1}, a...) // 在开头添加1个切片
+	fmt.Printf("a=%v len(a)=%d cap(a)=%d\n", a, len(a), cap(a))
 }
