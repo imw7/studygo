4ebca178a7dc5d9237218d0ca08b40284be7fd93
imw7 <hugew7@gmail.com>
2021-12-12 12:10:45 +0800

    rm files from repository

--- day15/blogger/static/js/nav.js
+++
@@ -1,9 +0,0 @@
-let obj;
-const As = document.getElementById("topnav").getElementsByTagName('a');
-obj = As[0];
-for (let i = 1; i < As.length; i++) {
-    if (window.location.href.indexOf(As[i].href) >= 0) {
-        obj = As[i];
-    }
-}
-obj.id = 'topnav_current'
