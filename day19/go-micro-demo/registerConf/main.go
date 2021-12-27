fe4719de0fa8f793803f74ed23374259349e5e79
imw7 <hugew7@gmail.com>
2021-12-27 14:03:51 +0800

    consul获取配置信息

--- day19/go-micro-demo/registerConf/main.go
+++ day19/go-micro-demo/registerConf/main.go
@@ -11,18 +11,29 @@ import (
 )
 
 func main() {
-	// 注册consul
+	// 1.注册consul
 	reg := consul.NewRegistry(func(options *registry.Options) {
 		options.Addrs = []string{"127.0.0.1:8500"}
 	})
 
-	// 配置中心
+	// 完成前4步之后，在consul管理页面的Key/Value创建micro/config目录，
+	// 然后在config目录创建mysql键，以json格式输入mysql配置的值：
+	// {
+	//	"host": "127.0.0.1",
+	//  "user": "root",
+	//  "pwd": "password",
+	//  "database": "sql_test",
+	//  "port": 3306
+	// }
+	// 具体操作可以在我的博客中找到：
+	// https://imw7.github.io/post/Go/go_micro/#%E6%B3%A8%E5%86%8C%E5%92%8C%E9%85%8D%E7%BD%AE%E4%B8%AD%E5%BF%83
+	// 5.配置中心
 	consulConfig, err := config.GetConsulConfig("127.0.0.1", 8500, "/micro/config")
 	if err != nil {
 		logger.Fatal(err)
 	}
 
-	// MySQL配置信息
+	// 6.获取MySQL配置信息
 	info, err := config.GetMySQLFromConsul(consulConfig, "mysql")
 	if err != nil {
 		logger.Fatal(err)
@@ -30,7 +41,7 @@ func main() {
 	// 必须使用logger才行，否则无法显示info信息
 	logger.Info("MySQL配置信息:", *info)
 
-	// 创建service
+	// 2.创建service
 	srv := micro.NewService(
 		micro.Name("registerConf"),
 		micro.Version("latest"),
@@ -38,13 +49,13 @@ func main() {
 		micro.Registry(reg),
 	)
 
-	// 注册handler
-	if err = pb.RegisterRegisterConfHandler(srv.Server(), &handler.RegisterConf{}); err != nil {
+	// 3.注册handler
+	if err := pb.RegisterRegisterConfHandler(srv.Server(), &handler.RegisterConf{}); err != nil {
 		logger.Fatal(err)
 	}
 
-	// 运行service
-	if err = srv.Run(); err != nil {
+	// 4.运行service
+	if err := srv.Run(); err != nil {
 		logger.Fatal(err)
 	}
 }
